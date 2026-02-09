35396550573258e7fff793b13061f3fc63663ff3
Pietro-H <pietro.henrique.pereira@escola.pr.gov.br>
2026-02-09 17:38:46 +0000

    primeira aula finalizada

--- calculaMedia.js
+++ calculaMedia.js
@@ -1,17 +1,29 @@
-let nota1 = 9;
-let nota2 = 3;
-let nota3 = 10;
-let nota4 = 6;
+let nota1 = 8;
+let nota2 = 5;
+let nota3 = 4;
+let nota4 = 1;
 
 const nomeAluno = "Bruninho Sigrles";
 
+const nomeDoColégio = "Reni Correia Gamper imforma que o estudante";
 
 let media = (nota1 + nota2 + nota3 +nota4) / 4
 
-
-
-console.log("O aluno: " + nomeAluno);
+console.log("Carissimos responsaveis o colégio " + nomeDoColégio);
+console.log(nomeAluno);
 console.log("Tem a média: " + media);
+
+
+if (media >= 7){
+    console.log("O aluno " + nomeAluno + " está aprovado! Parabéns!")
+} 
+if(media < 5){
+    console.log("O aluno " + nomeAluno + " está reprovado! Tente novamente ano que vem!")
+}
+if( media >= 5 && media < 7){
+    console.log("O aluno " + nomeAluno + " está no se liga! Mais cuidado!")
+}
+
 
 
 
