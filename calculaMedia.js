6631e0517d92596bb04cdb9aa3952cb595fd9d64
Pietro-H <pietro.henrique.pereira@escola.pr.gov.br>
2026-02-09 13:06:04 +0000

    primeira aula back-end

---
+++ calculaMedia.js
@@ -0,0 +1,17 @@
+let nota1 = 9;
+let nota2 = 3;
+let nota3 = 10;
+let nota4 = 6;
+
+const nomeAluno = "Bruninho Sigrles";
+
+
+let media = (nota1 + nota2 + nota3 +nota4) / 4
+
+
+
+console.log("O aluno: " + nomeAluno);
+console.log("Tem a média: " + media);
+
+
+
