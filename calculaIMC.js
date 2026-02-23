43865c7d2a12e2a997e6142e383f2446b14aa7cc
Pietro-H <pietro.henrique.pereira@escola.pr.gov.br>
2026-02-23 17:36:37 +0000

    aula04

---
+++ calculaIMC.js
@@ -0,0 +1,21 @@
+function calculaIMC(peso, altura){
+    let imc = peso / (altura ** 2);
+
+    console.log("seu IMC é, " + imc.toFixed (2));
+
+    if (imc <18.5){
+        console.log("clasificação: Abaixo do peso");
+    }
+    else if (imc >= 18.5 && imc <= 24.9){
+        console.log("Classificação: Peso normal");
+    }
+    else if (imc >= 25 && imc <= 29.9){
+        console.log("Classificação: Sobrepeso");
+    }
+    else {
+        console.log("Classificação: Obesidade");
+    }
+
+}
+
+calculaIMC(62, 1.79)
